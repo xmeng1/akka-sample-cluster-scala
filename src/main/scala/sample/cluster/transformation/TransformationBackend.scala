1d7755b65e499577062984923edba272a0db12f5
Xin Meng <mengxin.city@gmail.com>
2019-01-14 12:41:24 +0000

    initial

---
+++ src/main/scala/sample/cluster/transformation/TransformationBackend.scala
@@ -0,0 +1,52 @@
+package sample.cluster.transformation
+
+import language.postfixOps
+import scala.concurrent.duration._
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props, RootActorPath}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.CurrentClusterState
+import akka.cluster.ClusterEvent.MemberUp
+import akka.cluster.Member
+import akka.cluster.MemberStatus
+import com.typesafe.config.ConfigFactory
+
+//#backend
+class TransformationBackend extends Actor with ActorLogging {
+
+  val cluster = Cluster(context.system)
+
+  // subscribe to cluster changes, MemberUp
+  // re-subscribe when restart
+  override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  def receive = {
+    case TransformationJob(text) =>
+      log.info("Receive TransformationJob text {}", text)
+      sender() ! TransformationResult(text.toUpperCase)
+    case state: CurrentClusterState =>
+      state.members.filter(_.status == MemberStatus.Up) foreach register
+    case MemberUp(m) => register(m)
+  }
+
+  def register(member: Member): Unit =
+    if (member.hasRole("frontend"))
+      context.actorSelection(RootActorPath(member.address) / "user" / "frontend") !
+        BackendRegistration
+}
+//#backend
+
+object TransformationBackend {
+  def main(args: Array[String]): Unit = {
+    // Override the configuration of the port when specified as program argument
+    val port = if (args.isEmpty) "0" else args(0)
+    val config = ConfigFactory.parseString(s"""
+        akka.remote.artery.canonical.port=$port
+        """)
+      .withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]"))
+      .withFallback(ConfigFactory.load())
+
+    val system = ActorSystem("ClusterSystem", config)
+    system.actorOf(Props[TransformationBackend], name = "backend")
+  }
+}
